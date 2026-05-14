b84edd69e24fc8fcea62268df5b67318ecd2df60
tonniclaw <superywy@qq.com>
2026-05-14 17:47:19 +0800

    chore: clean generated and temporary files

--- test_rerun.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-try:
-    st.rerun()
-except Exception as e:
-    with open("rerun_catch.txt", "w") as f:
-        f.write("Caught Exception")
-except BaseException as e:
-    with open("rerun_catch.txt", "w") as f:
-        f.write("Caught BaseException")
--- test_rerun_runner.py
+++
@@ -1,3 +0,0 @@
-import sys
-from streamlit.runtime.scriptrunner.script_runner import RerunException
-print(issubclass(RerunException, Exception))
